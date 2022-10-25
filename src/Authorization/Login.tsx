7e2449dffa7432e8124fa22f47e3e3bbf1fa8bf0
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-25 10:14:42 +0300

    Rewrite page using mui grid

--- src/Authorization/Login.tsx
+++ src/Authorization/Login.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, TextField } from "@mui/material";
-import { logInWithEmailAndPassword } from "../firebase";
-import { Link } from "react-router-dom";
+import { Box, Button, Grid, TextField, Link } from "@mui/material";
+import { logInWithEmailAndPassword, signInWithGoogle } from "../firebase";
+import GoogleIcon from "@mui/icons-material/Google";
+import { useNavigate } from "react-router-dom";
 
 const Login = ({
   password,
@@ -13,47 +14,61 @@ const Login = ({
     logInWithEmailAndPassword(email, password, handleErrorMessage);
   };
 
+  const navigate = useNavigate();
+
   return (
-    <Box>
-      <Box
-        component="form"
-        noValidate
-        onSubmit={(e) => {
-          e.preventDefault();
-          login();
-        }}
-      >
-        <TextField
-          variant="outlined"
-          label="Email"
-          name="email"
-          fullWidth
-          margin="dense"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          variant="outlined"
-          label="Password"
-          name="password"
-          type="password"
-          fullWidth
-          margin="dense"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 2, mb: 2 }}
-        >
-          Sign In
-        </Button>
-      </Box>
-      <Link to="/register">Do not have an account? Register</Link>
+    <Box
+      component="form"
+      noValidate
+      onSubmit={(e) => {
+        e.preventDefault();
+        login();
+      }}
+      sx={{ mt: 3 }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            label="Email"
+            name="email"
+            fullWidth
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            label="Password"
+            name="password"
+            type="password"
+            fullWidth
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Button type="submit" fullWidth variant="contained">
+            Log In
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Button
+            fullWidth
+            variant="outlined"
+            onClick={() => signInWithGoogle(handleErrorMessage)}
+          >
+            <GoogleIcon />
+            Use Google
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Link onClick={() => navigate("/register")}>Sign up</Link>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
