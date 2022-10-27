424a711436d09a7be230966664810fa44a3512ae
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-27 18:13:09 +0300

    Add types

--- src/Snackbar/BasicSnackbar.tsx
+++ src/Snackbar/BasicSnackbar.tsx
@@ -1,5 +1,5 @@
 import React, { forwardRef } from "react";
-import Snackbar from "@mui/material/Snackbar";
+import Snackbar, { SnackbarCloseReason } from "@mui/material/Snackbar";
 import MuiAlert, { AlertProps } from "@mui/material/Alert";
 
 const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
@@ -9,7 +9,18 @@ const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-const BasicSnackbar = ({ open, onClose, message }) => {
+interface SnackbarProps {
+  open: boolean;
+  onClose:
+    | ((
+        event: Event | React.SyntheticEvent<any, Event>,
+        reason?: SnackbarCloseReason
+      ) => void)
+    | undefined;
+  message: string;
+}
+
+const BasicSnackbar = ({ open, onClose, message }: SnackbarProps) => {
   return (
     <>
       <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
