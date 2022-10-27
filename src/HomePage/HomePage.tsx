7fa36f57635ef2783703da54dc6e7b340aca66f5
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-27 18:12:38 +0300

    Remove unused code

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase/firebase";
 
 const HomePage = () => {
-  const [user, loading, error] = useAuthState(auth);
+  const [user] = useAuthState(auth);
   const navigate = useNavigate();
 
   useEffect(() => {
