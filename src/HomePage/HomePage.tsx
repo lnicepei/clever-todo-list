f9f83efe82e45aacf4f3ddaa4e9a9c3f81391ab3
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-29 10:19:52 +0300

    Create basic markup

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase/firebase";
+import { Button, Grid, Typography } from "@mui/material";
+import logo from "./todo-logo.jpg";
 
 const HomePage = () => {
   const [user] = useAuthState(auth);
@@ -11,7 +13,29 @@ const HomePage = () => {
     if (user) navigate("/tasks");
   }, [user]);
 
-  return <div>HomePage</div>;
+  return (
+    <Grid container spacing={2}>
+      <Grid
+        item
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        xs={5}
+      >
+        <Typography variant="h1" gutterBottom sx={{ fontWeight: 700 }}>
+          Hi there
+        </Typography>
+        <Button variant="contained" onClick={() => navigate("/login")}>Log In</Button>
+        <Button variant="contained" onClick={() => navigate("/register")}>Register</Button>
+      </Grid>
+      <Grid item xs={7}>
+        <img src={logo} alt="" style={{ height: "90vh" }} />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default HomePage;
