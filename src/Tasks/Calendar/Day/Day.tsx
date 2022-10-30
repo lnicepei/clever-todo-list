4890ca277c0f0074db37fa05ce7118680371a89b
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-30 17:59:59 +0300

    Set styles for days

--- src/Tasks/Calendar/Day/Day.tsx
+++ src/Tasks/Calendar/Day/Day.tsx
@@ -1,28 +1,40 @@
 import { Card } from "@mui/material";
+import { format, isPast, isToday } from "date-fns";
 import { MutableRefObject } from "react";
 
 interface DayProps {
   day: string;
   onClick: () => false | undefined;
   selected: boolean;
-  todayRef: MutableRefObject<HTMLDivElement | null> | null;
+  dayRef: MutableRefObject<HTMLDivElement | null>;
+  date: Date;
 }
 
-const Day = ({ day, onClick, selected, todayRef }: DayProps) => {
+const Day = ({ day, onClick, selected, dayRef, date }: DayProps) => {
   return (
     <Card
       variant="outlined"
       onClick={() => onClick()}
       sx={{
-        width: "15vh",
-        height: "10vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        width: "7vh",
+        height: "8vh",
+        margin: "5px",
+        borderRadius: "10px",
         cursor: "pointer",
         userSelect: "none",
-        backgroundColor: (selected && "red") || "",
+        backgroundColor:
+          (isToday(date) && "#1976d2") || (isPast(date) && "#eaebed") || "",
+        outline: (selected && "2px solid red") || "",
+        color: (!isToday(date) && isPast(date) && "#524e4f") || "",
       }}
-      ref={todayRef}
+      ref={dayRef}
     >
-      {day}
+      <div>{format(new Date(day), "E")}</div>
+      <div>{format(new Date(day), "d")}</div>
     </Card>
   );
 };
