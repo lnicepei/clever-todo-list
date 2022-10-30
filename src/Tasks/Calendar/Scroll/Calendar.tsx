612e7a2f3bfdfb6eb46d7d78a7627208138f4d16
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-30 12:30:12 +0300

    Implement infinite scroll

--- src/Tasks/Calendar/Scroll/Calendar.tsx
+++ src/Tasks/Calendar/Scroll/Calendar.tsx
@@ -10,34 +10,48 @@ import { TasksContext } from "../../Tasks";
 type scrollVisibilityApiType = React.ContextType<typeof VisibilityContext>;
 
 const Calendar = () => {
-  const tasksContext = useContext(TasksContext);
-  const calendar: string[] = [];
   const startDay = startOfMonth(new Date());
-  const endDay = endOfMonth(new Date());
-
-  const todayRef = useRef<null | HTMLDivElement>(null);
-
+  let endDay = endOfMonth(new Date());
   let day = subDays(startDay, 1);
-
-  while (isBefore(day, endDay)) {
-    calendar.push(addDays(day, 1).toDateString());
-    day = addDays(day, 1);
-  }
-
+  const tempCalendar: string[] = [];
   const { dragStart, dragStop, dragMove, dragging } = useDrag();
+  const tasksContext = useContext(TasksContext);
+  const todayRef = useRef<null | HTMLDivElement>(null);
+  const scrollMenuRef = useRef({} as scrollVisibilityApiType);
+  const [calendar, setCalendar] = useState<string[]>([]);
+  const [selected, setSelected] = useState<number>(
+    tempCalendar.indexOf(new Date().toDateString())
+  );
+
+  const updateCalendar = () => {
+    if (
+      scrollMenuRef.current.visibleElements.includes(
+        scrollMenuRef.current.items.toItems().at(-5) || ""
+      )
+    ) {
+      tempCalendar.length = 0;
+      endDay = addDays(endDay, 30);
+      add30DaysToCalendar();
+      setCalendar((prevCalendar) => prevCalendar.concat(tempCalendar));
+    }
+  };
+
+  const add30DaysToCalendar = () => {
+    while (isBefore(day, endDay)) {
+      tempCalendar.push(addDays(day, 1).toDateString());
+      day = addDays(day, 1);
+    }
+  };
 
   const handleDrag =
     ({ scrollContainer }: scrollVisibilityApiType) =>
     (ev: React.MouseEvent) =>
       dragMove(ev, (posDiff) => {
         if (scrollContainer.current) {
+          updateCalendar();
           scrollContainer.current.scrollLeft += posDiff;
         }
       });
-
-  const [selected, setSelected] = useState<number>(
-    calendar.indexOf(new Date().toDateString())
-  );
 
   const handleItemClick = (key: number) => () => {
     if (dragging) {
@@ -49,27 +63,31 @@ const Calendar = () => {
   };
 
   useEffect(() => {
+    add30DaysToCalendar();
+    setCalendar(tempCalendar);
+  }, []);
+
+  useEffect(() => {
     todayRef?.current?.scrollIntoView({ behavior: "smooth", inline: "center" });
   }, []);
 
   return (
-    <div style={{ overflow: "hidden" }} onMouseLeave={dragStop}>
-      <ScrollMenu
-        onMouseDown={() => dragStart}
-        onMouseUp={() => dragStop}
-        onMouseMove={handleDrag}
-      >
-        {calendar.map((dayOfMonth, key) => (
-          <Day
-            day={dayOfMonth}
-            key={key}
-            onClick={handleItemClick(key)}
-            selected={key === selected}
-            todayRef={key === selected ? todayRef : null}
-          />
-        ))}
-      </ScrollMenu>
-    </div>
+    <ScrollMenu
+      onMouseDown={() => dragStart}
+      onMouseUp={() => dragStop}
+      onMouseMove={handleDrag}
+      apiRef={scrollMenuRef}
+    >
+      {calendar.map((dayOfMonth, key) => (
+        <Day
+          day={dayOfMonth}
+          key={key}
+          onClick={handleItemClick(key)}
+          selected={key === selected}
+          todayRef={key === selected ? todayRef : null}
+        />
+      ))}
+    </ScrollMenu>
   );
 };
 
