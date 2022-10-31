c9380728f5c39710f1cd70796bcd2ee9fedf1e72
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-31 18:54:30 +0300

    Clear up the code

--- src/Tasks/Calendar/Scroll/Calendar.tsx
+++ src/Tasks/Calendar/Scroll/Calendar.tsx
@@ -1,14 +1,6 @@
-import {
-  addDays,
-  addMonths, isBefore, subDays
-} from "date-fns";
+import { addDays, addMonths, isBefore, subDays } from "date-fns";
 import { endOfMonth } from "date-fns/esm";
-import React, {
-  useContext,
-  useEffect,
-  useRef,
-  useState
-} from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { TasksContext } from "../../Tasks";
 import Day from "../Day/Day";
@@ -21,7 +13,6 @@ const Calendar = () => {
   const [startDay, setStartDay] = useState(new Date());
   const [endDay, setEndDay] = useState(endOfMonth(new Date()));
   let day = subDays(startDay, 1);
-  let dayToPrepend = subDays(endDay, 1);
 
   let tempCalendar: string[] = [];
 
@@ -47,46 +38,19 @@ const Calendar = () => {
     setCalendar(tempCalendar);
   };
 
-  // const prependMonthToCalendar = () => {
-  //   setStartDay((prevStartDay) => subMonths(prevStartDay, 1));
-
-  //   while (isBefore(startDay, dayToPrepend)) {
-  //     tempCalendar.unshift(subDays(dayToPrepend, 1).toDateString());
-  //     dayToPrepend = subDays(dayToPrepend, 1);
-  //   }
-
-  //   setCalendar(tempCalendar);
-  // };
-
-  // useEffect(() => {
-  //   if (
-  //     scrollMenuRef.current.visibleElements.includes(
-  //       scrollMenuRef.current.items.toItems()[4] || ""
-  //     )
-  //   )
-  //     prependMonthToCalendar();
-  //   scrollMenuRef.current.scrollToItem(
-  //     scrollMenuRef?.current?.items.get(new Date().getDate().toString())
-  //   );
-  // }, [scrollMenuRef.current.isFirstItemVisible]);
-
-  // useEffect(() => {
-  //   if (scrollMenuRef.current.isFirstItemVisible) prependMonthToCalendar();
-  // }, [scrollMenuRef.current.isFirstItemVisible]);
-
-  useEffect(() => {
-    if (scrollMenuRef.current.isLastItemVisible) appendMonthToCalendar();
-  }, [scrollMenuRef.current.isLastItemVisible]);
-
   const handleDrag =
     ({ scrollContainer }: scrollVisibilityApiType) =>
     (ev: React.MouseEvent) =>
       dragMove(ev, (posDiff) => {
-        if (scrollContainer.current) {
+        if (
+          scrollContainer.current &&
+          scrollMenuRef.current.items.toArr().at(-1)
+        ) {
           if (scrollMenuRef.current.items.toArr().at(-1)[1].visible === true) {
             appendMonthToCalendar();
             scrollMenuRef.current.items.toArr().at(-1)[1].visible = false;
           }
+
           scrollContainer.current.scrollLeft += posDiff;
         }
       });
@@ -101,8 +65,6 @@ const Calendar = () => {
   };
 
   useEffect(() => {
-    // setEndDay((prevEndDay) => addMonths(prevEndDay, 1));
-    // setStartDay((prevStartDay) => subMonths(prevStartDay, 1));
     appendMonthToCalendar();
     setCalendar(tempCalendar);
   }, []);
@@ -113,30 +75,18 @@ const Calendar = () => {
       onMouseUp={() => dragStop}
       onMouseMove={handleDrag}
       apiRef={scrollMenuRef}
-      // onInit={() =>
-      // scrollMenuRef.current.scrollToItem(
-      //   scrollMenuRef?.current?.items.get(new Date().getDate().toString())
-      // )
-      // }
-      onInit={
-        () => appendMonthToCalendar()
-        //   prependMonthToCalendar() ||
-        //   scrollMenuRef.current.scrollToItem(
-        //     scrollMenuRef?.current?.items.get(new Date().getDate().toString())
-        //   )
-      }
+      onInit={() => appendMonthToCalendar()}
     >
-      {console.log(calendar) ||
-        calendar.map((dayOfMonth, key) => (
-          <Day
-            day={dayOfMonth}
-            key={key}
-            onClick={handleItemClick(key)}
-            selected={key === selected}
-            dayRef={dayRef}
-            date={new Date(calendar?.at(key) ?? "")}
-          />
-        ))}
+      {calendar.map((dayOfMonth, key) => (
+        <Day
+          day={dayOfMonth}
+          key={key}
+          onClick={handleItemClick(key)}
+          selected={key === selected}
+          dayRef={dayRef}
+          date={new Date(calendar?.at(key) ?? "")}
+        />
+      ))}
     </ScrollMenu>
   );
 };
