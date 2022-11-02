7fdb34f9b5f7f7cc0fbacf53e4848ec66d349c46
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-02 15:06:14 +0300

    Add functions to handle scroll by touch

--- src/Tasks/Calendar/ScrollableCalendar/UseDrag.ts
+++ src/Tasks/Calendar/ScrollableCalendar/UseDrag.ts
@@ -5,6 +5,11 @@ const useDrag = () => {
   const [dragging, setDragging] = useState(false);
   const position = useRef<number>(0);
 
+  const touchStart = useCallback((event: React.TouchEvent) => {
+    position.current = event.changedTouches[0].clientX;
+    setClicked(true);
+  }, []);
+
   const dragStart = useCallback((event: React.MouseEvent) => {
     position.current = event.clientX;
     setClicked(true);
@@ -23,7 +28,6 @@ const useDrag = () => {
   const dragMove = (event: React.MouseEvent, cb: (posDif: number) => void) => {
     const newDiff = position.current - event.clientX;
     const movedEnough = Math.abs(newDiff) > 5;
-
     if (clicked && movedEnough) {
       setDragging(true);
     }
@@ -34,10 +38,28 @@ const useDrag = () => {
     }
   };
 
+  const dragMoveTouch = (
+    event: React.TouchEvent,
+    cb: (posDif: number) => void
+  ) => {
+    const newDiff = position.current - event.changedTouches[0].clientX;
+    const movedEnough = Math.abs(newDiff) > 5;
+    if (clicked && movedEnough) {
+      setDragging(true);
+    }
+
+    if (dragging && movedEnough) {
+      position.current = event.changedTouches[0].clientX;
+      cb(newDiff);
+    }
+  };
+
   return {
     dragStart,
+    touchStart,
     dragStop,
     dragMove,
+    dragMoveTouch,
     dragging,
     position,
     setDragging,
