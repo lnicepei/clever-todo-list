949ef952e9669fe0b490b7a83726dea2d4844da6
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-02 10:37:37 +0300

    Implement task edit

--- src/Tasks/TasksView/Task/Task.tsx
+++ src/Tasks/TasksView/Task/Task.tsx
@@ -1,10 +1,19 @@
-import { Card, CardContent, Checkbox, Typography } from "@mui/material";
+import { DeleteForever, Edit, MoreHoriz } from "@mui/icons-material";
+import {
+  Card,
+  CardContent,
+  Checkbox,
+  Fade,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import { format } from "date-fns";
 import { doc, setDoc } from "firebase/firestore";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { db } from "../../../firebase/firebase";
 import { TasksContext } from "../../Tasks";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 declare global {
   interface Task {
@@ -15,11 +24,11 @@ declare global {
   }
 }
 
-interface TaskProps {
+type TaskProps = {
   task: Task;
-}
+};
 
-const Task = ({ task }: TaskProps) => {
+const Task: React.FC<TaskProps> = ({ task }) => {
   const tasksContext = useContext(TasksContext);
   const toggleComplete = async () => {
     tasksContext!.setAllTasks((prevTasks) =>
@@ -57,26 +66,75 @@ const Task = ({ task }: TaskProps) => {
         }
       }),
     });
+
+    handleClose();
+  };
+
+  const editTask = async () => {
+    tasksContext!.setTaskContent(task);
+    tasksContext!.setOpen((prevOpen) => !prevOpen);
+
+    handleClose();
+  };
+
+  const [anchorElTask, setAnchorElTask] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorElTask);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElTask(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorElTask(null);
   };
 
   return (
-    <Card
-      sx={{
-        color: "#333",
-        backgroundColor: "#c3d9d9",
-        boxShadow:
-          "-5px -5px 9px rgba(255, 255, 255, 0.45), 5px 5px 9px rgba(94, 104, 121, 0.3)",
-        cursor: "pointer",
-      }}
-    >
-      <CardContent>
+    <Card>
+      <CardContent sx={{ display: "flex" }}>
         <Checkbox checked={task.complete} onChange={toggleComplete}></Checkbox>
-        <DeleteForeverIcon onClick={deleteTask} />
         <Typography gutterBottom>{task.name}</Typography>
         <Typography gutterBottom>{`${format(
           new Date(task.date),
           "MMM"
         )}`}</Typography>
+        <IconButton
+          id="fade-button"
+          aria-controls={open ? "fade-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <MoreHoriz />
+        </IconButton>
+        <Menu
+          id="fade-menu"
+          MenuListProps={{
+            "aria-labelledby": "fade-button",
+          }}
+          anchorEl={anchorElTask}
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
+        >
+          <MenuItem onClick={deleteTask}>
+            <Typography
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              <DeleteForever />
+              Delete
+            </Typography>
+          </MenuItem>
+          <MenuItem onClick={editTask}>
+            <Typography sx={{ display: "flex", alignItems: "center" }}>
+              <Edit />
+              Edit
+            </Typography>
+          </MenuItem>
+        </Menu>
       </CardContent>
     </Card>
   );
