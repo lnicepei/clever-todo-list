46ef6ad74e2fc3486777685abc99f3a7c553a2d8
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-04 18:51:20 +0300

    Wrap router in themeProvider

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,5 @@
-import { Container } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 import {
   BrowserRouter as Router,
   Navigate,
@@ -11,35 +12,45 @@ import Register from "../Authorization/Register/Register";
 import HomePage from "../HomePage/HomePage";
 import NotFound from "../NotFound/NotFound";
 import Tasks from "../Tasks/Tasks";
+import { darkTheme, lightTheme } from "../themes/Themes";
 import "./App.css";
 
 function App() {
   return (
     <Container sx={{ padding: 0 }}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="login"
-            element={
-              <Auth>
-                <Login />
-              </Auth>
-            }
-          ></Route>
-          <Route
-            path="register"
-            element={
-              <Auth>
-                <Register />
-              </Auth>
-            }
-          ></Route>
-          <Route path="tasks" element={<Tasks />} />
-          <Route path="404" element={<NotFound />} />
-          <Route path="*" element={<Navigate to="/404" />} />
-        </Routes>
-      </Router>
+      <ThemeProvider
+        theme={
+          window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? darkTheme
+            : lightTheme
+        }
+      >
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="login"
+              element={
+                <Auth>
+                  <Login />
+                </Auth>
+              }
+            ></Route>
+            <Route
+              path="register"
+              element={
+                <Auth>
+                  <Register />
+                </Auth>
+              }
+            ></Route>
+            <Route path="tasks" element={<Tasks />} />
+            <Route path="404" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/404" />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </Container>
   );
 }
