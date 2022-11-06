a2fb4cd85db0f233f70d22e2e21c77e8f06320e2
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-06 09:18:58 +0300

    Handle wheel scroll

--- src/Tasks/Calendar/ScrollableCalendar/ScrollableCalendar.tsx
+++ src/Tasks/Calendar/ScrollableCalendar/ScrollableCalendar.tsx
@@ -2,11 +2,12 @@ import { Stack } from "@mui/system";
 import {
   addDays,
   addMonths,
-  isAfter,
   isBefore,
   subDays,
   endOfMonth,
   subMonths,
+  isAfter,
+  startOfMonth,
 } from "date-fns";
 import { useContext, useEffect, useRef, useState } from "react";
 import { TasksContext } from "../../Tasks";
@@ -14,45 +15,88 @@ import Day from "../Day/Day";
 import useDrag from "./UseDrag";
 
 const ScrollableCalendar = () => {
-  const { dragStart, touchStart, dragStop, dragMove, dragMoveTouch, dragging } =
-    useDrag();
+  const {
+    dragStart,
+    touchStart,
+    dragStop,
+    dragMove,
+    touchMove,
+    dragging,
+    wheelMove,
+  } = useDrag();
   const tasksContext = useContext(TasksContext);
 
-  const [startDay, setStartDay] = useState(subMonths(new Date(), 0));
-  const [endDay, setEndDay] = useState(addMonths(endOfMonth(new Date()), 1));
+  // const [startDay, setStartDay] = useState(startOfMonth(new Date()));
+  // let startDay = startOfMonth(new Date());
+  // const startDay = subMonths(new Date(), 0);
+  // const [endDay, setEndDay] = useState(addMonths(endOfMonth(new Date()), 1));
+  // let endDay = endOfMonth(new Date());
 
-  let dayToAppend = subDays(startDay, 1);
-  let dayToPrepend = addDays(startDay, 1);
+  // let dayToPrepend = addDays(startDay, 1);
 
-  let tempCalendar: string[] = [];
+  // let tempCalendar: string[] = [];
 
   const dayRef = useRef<null | HTMLDivElement>(null);
   const scrollMenuRef = useRef<null | HTMLDivElement>(null);
   const initialDayRef = useRef<null | HTMLDivElement>(null);
+  // const widthRef = useRef<number>(0);
 
   const [calendar, setCalendar] = useState<string[]>([]);
   const [selected, setSelected] = useState<number>(0);
 
-  const appendMonthToCalendar = () => {
-    setEndDay((prevEndDay) => addMonths(prevEndDay, 1));
+  const setInitialCalendar = () => {
+    const tempCalendar: string[] = [];
+    let startDay = startOfMonth(subMonths(new Date(), 0));
 
-    while (isBefore(dayToAppend, endDay)) {
-      tempCalendar.push(addDays(dayToAppend, 1).toDateString());
-      dayToAppend = addDays(dayToAppend, 1);
+    while (isBefore(startDay, endOfMonth(new Date()))) {
+      tempCalendar.push(startDay.toDateString());
+      startDay = addDays(startDay, 1);
     }
 
     setCalendar(tempCalendar);
   };
 
+  const appendMonthToCalendar = () => {
+    const tempCalendar: string[] = [];
+    // let endDay = new Date(calendar.at(-1));
+    let dayToAppend = new Date(calendar?.at(-1) ?? "");
+    let endDay = addMonths(dayToAppend, 1);
+
+    while (isBefore(dayToAppend, endDay)) {
+      tempCalendar.push(addDays(dayToAppend, 1).toDateString());
+      dayToAppend = addDays(dayToAppend, 1);
+    }
+    //display fixed for parent element
+    console.log("TempCalendar", tempCalendar);
+
+    // console.log("TempCalendar: ", tempCalendar);
+
+    setCalendar((prevCalendar) => prevCalendar.concat(tempCalendar));
+  };
+
+  // const prependMonthToCalendar = () => {
+  // // setStartDay((prevStartDay) => subMonths(prevStartDay, 1));
+  // startDay = subMonths(startDay, 1);
+  // const tempCalendar: string[] = [];
+  // console.log(dayToPrepend, subMonths(startDay, 1));
+  // while (isAfter(dayToPrepend, startDay)) {
+  //   tempCalendar.unshift(subDays(dayToPrepend, 1).toDateString());
+  //   dayToPrepend = subDays(dayToPrepend, 1);
+  // }
+  // setCalendar((prevCalendar) => prevCalendar.concat(tempCalendar));
+  // };
+
   const prependMonthToCalendar = () => {
-    setStartDay((prevStartDay) => subMonths(prevStartDay, 1));
+    const tempCalendar: string[] = [];
+    let dayToPrepend = new Date(calendar?.at(0) ?? "");
+    let startDay = subMonths(dayToPrepend, 1);
 
     while (isAfter(dayToPrepend, startDay)) {
       tempCalendar.unshift(subDays(dayToPrepend, 1).toDateString());
       dayToPrepend = subDays(dayToPrepend, 1);
     }
 
-    setCalendar(tempCalendar);
+    setCalendar((prevCalendar) => tempCalendar.concat(prevCalendar));
   };
 
   const dragAction = (positionDifference: number) => {
@@ -66,18 +110,16 @@ const ScrollableCalendar = () => {
         appendMonthToCalendar();
       }
 
-      // if (scrollMenuRef.current.scrollLeft === 0) {
-      //   scrollMenuRef.current.scrollLeft += scrollMenuRef.current.scrollWidth;
-      //   prependMonthToCalendar();
-      // }
-
-      console.log(
-        scrollMenuRef.current.scrollLeft,
-        scrollMenuRef.current.clientWidth,
-        scrollMenuRef.current?.scrollWidth
-      );
+      if (scrollMenuRef.current.scrollLeft === 0) {
+        prependMonthToCalendar();
+        scrollMenuRef.current.scrollLeft += 1308;
+      }
     }
   };
+
+  // useEffect(() => {
+  //   scrollMenuRef.current.scrollLeft += 1308;
+  // }, [calendar.length]);
 
   const handleItemClick = (key: number) => () => {
     if (dragging) {
@@ -89,18 +131,19 @@ const ScrollableCalendar = () => {
   };
 
   useEffect(() => {
-    appendMonthToCalendar();
-    setSelected(tempCalendar.indexOf(new Date().toDateString()));
+    // appendMonthToCalendar();
+    setInitialCalendar();
     // prependMonthToCalendar();
-    setCalendar(tempCalendar);
+    // setSelected(tempCalendar.indexOf(new Date().toDateString()));
+    // setCalendar(tempCalendar);
   }, []);
 
-  useEffect(() => {
-    initialDayRef.current?.scrollIntoView({
-      behavior: "smooth",
-      inline: "center",
-    });
-  }, [initialDayRef.current]);
+  // useEffect(() => {
+  //   initialDayRef.current?.scrollIntoView({
+  //     behavior: "smooth",
+  //     inline: "center",
+  //   });
+  // }, [initialDayRef.current]);
 
   return (
     <Stack
@@ -108,8 +151,9 @@ const ScrollableCalendar = () => {
       onMouseUp={dragStop}
       onTouchStart={touchStart}
       onTouchEnd={dragStop}
-      onTouchMove={(e) => dragMoveTouch(e, dragAction)}
+      onTouchMove={(e) => touchMove(e, dragAction)}
       onMouseMove={(e) => dragMove(e, dragAction)}
+      onWheel={(e) => wheelMove(e, dragAction)}
       ref={scrollMenuRef}
       style={{ overflow: "hidden", padding: "10px 0" }}
       direction="row"
