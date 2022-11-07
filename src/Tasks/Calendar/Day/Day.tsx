6eaef7a08c4c368ff3cd09b70a0f3785495afd22
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-07 12:29:55 +0300

    Move optional styles from jsx to component body

--- src/Tasks/Calendar/Day/Day.tsx
+++ src/Tasks/Calendar/Day/Day.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Container, Typography } from "@mui/material";
 import { format, isPast, isToday } from "date-fns";
 import { MutableRefObject, useContext } from "react";
 import { TasksContext } from "../../Tasks";
@@ -15,67 +15,53 @@ type DayProps = {
 const Day: React.FC<DayProps> = ({ day, onClick, selected, dayRef, date }) => {
   const tasksContext = useContext(TasksContext);
 
+  const formattedMonth = format(new Date(day), "E");
+  const formattedDay = format(new Date(day), "d");
+
+  const taskList =
+    tasksContext?.allTasks.some(
+      (task) => task.date.substring(0, day.length) === day
+    ) &&
+    (tasksContext?.allTasks
+      .filter((task) => task.date.substring(0, day.length) === day)
+      .some((task) => !task.complete) ? (
+      tasksContext?.allTasks
+        .filter((task) => task.date.substring(0, day.length) === day)
+        .some((task) => task.complete) ? (
+        <ul className="markers both">
+          <li>&nbsp; &nbsp;</li>
+          <li></li>
+        </ul>
+      ) : (
+        <ul className="markers undone">
+          <li>&nbsp; &nbsp;</li>
+        </ul>
+      )
+    ) : (
+      <ul className="markers done">
+        <li>&nbsp; &nbsp;</li>
+      </ul>
+    ));
+
+  const cardStyles = {
+    bgcolor:
+      (isToday(date) && "secondary.main") ||
+      (isPast(date) && "secondary.light") ||
+      "secondary.dark",
+    outline: (selected && "2px solid") || "none",
+    color:
+      (selected && "warning.dark") ||
+      (isToday(date) && "primary.light") ||
+      (!isToday(date) && isPast(date) && "text.disabled") ||
+      "common.white",
+  };
+
   return (
-    <Card
-      variant="outlined"
-      onClick={() => onClick()}
-      ref={dayRef}
-      sx={{
-        position: "relative",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: "18px",
-        margin: "0 5px",
-        overflow: "visible",
-        userSelect: "none",
-        boxSizing: "border-box",
-        backgroundColor:
-          (isToday(date) && "black") || (isPast(date) && "#797a7c33") || "",
-        outline: (selected && "2px solid #ffa781") || "",
-        color:
-          (selected && "#ff8181") ||
-          (isToday(date) && "white") ||
-          (!isToday(date) && isPast(date) && "#898687") ||
-          "",
-      }}
-    >
-      <CardContent
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-          cursor: "pointer",
-          height: "65px",
-          width: "55px",
-        }}
-      >
-        <Typography>{format(new Date(day), "E")}</Typography>
-        <Typography>{format(new Date(day), "d")}</Typography>
-        {tasksContext?.allTasks.some(
-          (task) => task.date.substring(0, day.length) === day
-        ) &&
-          (tasksContext?.allTasks
-            .filter((task) => task.date.substring(0, day.length) === day)
-            .some((task) => !task.complete) ? (
-            tasksContext?.allTasks
-              .filter((task) => task.date.substring(0, day.length) === day)
-              .some((task) => task.complete) ? (
-              <ul className="markers both">
-                <li>&nbsp; &nbsp;</li>
-                <li></li>
-              </ul>
-            ) : (
-              <ul className="markers undone">
-                <li>&nbsp; &nbsp;</li>
-              </ul>
-            )
-          ) : (
-            <ul className="markers done">
-              <li>&nbsp; &nbsp;</li>
-            </ul>
-          ))}
+    <Card variant="outlined" onClick={onClick} ref={dayRef} sx={cardStyles}>
+      <CardContent>
+        <Typography>{formattedMonth}</Typography>
+        <Typography>{formattedDay}</Typography>
+        <Container>{taskList}</Container>
       </CardContent>
     </Card>
   );
