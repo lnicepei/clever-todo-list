63284099b84a5f46fdaebac80a077dd0e7f91d80
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-07 14:23:19 +0300

    Move api-related code to firebase folder

--- src/Tasks/Tasks.tsx
+++ src/Tasks/Tasks.tsx
@@ -1,28 +1,19 @@
-import { isPast, isSameDay, isToday } from "date-fns";
 import { User } from "firebase/auth";
-import {
-  collection,
-  DocumentData,
-  getDocs,
-  query,
-  QueryDocumentSnapshot,
-  where,
-} from "firebase/firestore";
+import { DocumentData, QueryDocumentSnapshot } from "firebase/firestore";
 import { nanoid } from "nanoid";
 import { createContext, useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
-import { auth, db } from "../firebase/firebase";
+import { auth, fetchUserData } from "../firebase/firebase";
 import ScrollableCalendar from "./Calendar/ScrollableCalendar/ScrollableCalendar";
 import NewTask from "./NewTask/NewTaskWrapper/NewTaskWrapper";
 import ResponsiveAppBar from "./ResponsiveAppBar/ResponsiveAppBar";
-import TaskView from "./TasksView/TaskWrapper/TaskView";
+import TaskView from "./TasksView/TaskWrapper/TaskWrapper";
 
 interface TasksContext {
   name: string;
   user: User | null | undefined;
   userFromDB: QueryDocumentSnapshot<DocumentData> | undefined;
-  tasksFromDay: Task[];
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   taskContent: Task;
@@ -41,7 +32,6 @@ const Tasks = () => {
     useState<QueryDocumentSnapshot<DocumentData>>();
   const [name, setName] = useState("");
   const [allTasks, setAllTasks] = useState<Task[]>([]);
-  const [tasksFromDay, setTasksFromDay] = useState<Task[]>([]);
   const [dayToShowTasks, setDayToShowTasks] = useState(
     new Date().toDateString()
   );
@@ -55,27 +45,10 @@ const Tasks = () => {
 
   const navigate = useNavigate();
 
-  const fetchUserData = async () => {
-    const q = query(collection(db, "users"), where("uid", "==", user?.uid));
-    const doc = await getDocs(q);
-    const data = doc.docs[0].data();
-    setUserFromDB(doc.docs[0]);
-    setAllTasks(data.tasks);
-    setName(data.name || (user?.displayName ?? user?.email));
-  };
-
-  useEffect(() => {
-    setTasksFromDay(
-      allTasks.filter((task: Task) =>
-        isSameDay(new Date(task.date), new Date(dayToShowTasks))
-      )
-    );
-  }, [dayToShowTasks, allTasks]);
-
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/");
-    fetchUserData();
+    fetchUserData(user, setUserFromDB, setAllTasks, setName);
   }, [user, loading]);
 
   return (
@@ -84,7 +57,6 @@ const Tasks = () => {
         name,
         user,
         userFromDB,
-        tasksFromDay,
         open,
         setOpen,
         taskContent,
