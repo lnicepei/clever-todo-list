d773c48d8a570803e93eb6f050aa62e1559574cc
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-09 15:57:55 +0300

    Move states to reducer

--- src/Tasks/Tasks.tsx
+++ src/Tasks/Tasks.tsx
@@ -1,54 +1,23 @@
-import { User } from "firebase/auth";
-import { DocumentData, QueryDocumentSnapshot } from "firebase/firestore";
-import { nanoid } from "nanoid";
-import { createContext, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth, fetchUserData } from "../firebase/firebase";
 import ScrollableCalendar from "./Calendar/ScrollableCalendar/ScrollableCalendar";
 import ResponsiveAppBar from "./ResponsiveAppBar/ResponsiveAppBar";
+import { useTasksDispatch } from "./TasksContext";
 import StyledPuffLoader from "./TasksView/StyledPuffLoader/StyledPuffLoader";
 import TaskView from "./TasksView/TaskWrapper/TaskWrapper";
 
-interface TasksContext {
-  name: string;
-  user: User | null | undefined;
-  userFromDB: QueryDocumentSnapshot<DocumentData> | undefined;
-  open: boolean;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  taskContent: Task;
-  setTaskContent: React.Dispatch<React.SetStateAction<Task>>;
-  allTasks: Task[];
-  setAllTasks: React.Dispatch<React.SetStateAction<Task[]>>;
-  dayToShowTasks: string;
-  setDayToShowTasks: React.Dispatch<React.SetStateAction<string>>;
-}
-
-export const TasksContext = createContext<TasksContext | null>(null);
-
 const Tasks = () => {
   const [user, loading] = useAuthState(auth);
-  const [userFromDB, setUserFromDB] =
-    useState<QueryDocumentSnapshot<DocumentData>>();
-  const [name, setName] = useState("");
-  const [allTasks, setAllTasks] = useState<Task[]>([]);
-  const [dayToShowTasks, setDayToShowTasks] = useState(
-    new Date().toDateString()
-  );
-  const [open, setOpen] = useState(false);
-  const [taskContent, setTaskContent] = useState<Task>({
-    name: "",
-    date: dayToShowTasks,
-    complete: false,
-    id: nanoid(),
-  });
+  const dispatch = useTasksDispatch();
 
   const navigate = useNavigate();
 
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/");
-    fetchUserData(user, setUserFromDB, setAllTasks, setName);
+    fetchUserData(user, dispatch);
   }, [user, loading]);
 
   return (
@@ -56,25 +25,11 @@ const Tasks = () => {
       {loading ? (
         <StyledPuffLoader />
       ) : (
-        <TasksContext.Provider
-          value={{
-            name,
-            user,
-            userFromDB,
-            open,
-            setOpen,
-            taskContent,
-            setTaskContent,
-            allTasks,
-            setAllTasks,
-            dayToShowTasks,
-            setDayToShowTasks,
-          }}
-        >
+        <>
           <ResponsiveAppBar />
           <ScrollableCalendar />
           <TaskView />
-        </TasksContext.Provider>
+        </>
       )}
     </>
   );
